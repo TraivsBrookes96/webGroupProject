7fb1d444a1b6ddc174ca820bbaef517cce0ae1ca
TraivsBrookes96 <tbrookes96@gmail.com>
2017-03-15 09:22:55 +0000

    LogIn Script Changed - Fayaz

--- group_project/website/templates/login-template.php
+++ group_project/website/templates/login-template.php
@@ -1,15 +1,63 @@
-<div id="LogIn">
-  <form action="home" method="POST">
-    <p> Please use your details to Log In</p>
-    <br>
-    <label>Username </label><input type="text" name="user_name"/>
-    <br>
-    <br>
-    <label>Password </label><input type="password" name="password"/>
-    <br>
-    <br>
-    <input type="submit" value="Log In" name="submit"/>
-  </form>
-</div>
-<div id="HomeVideo">
-</div>
+<?php
+session_start();
+require '../database/databaseconnection.php';
+if (isset($_POST['login']) ) {
+
+  $stmt = $pdo->prepare('SELECT * FROM students WHERE student_id = :student_id');
+  $criteria = [
+      'student_id' => $_POST['student_id']
+  ];
+  $stmt->execute($criteria);
+  $students= $stmt->fetch();
+
+  if (password_verify($_POST['password'], $students['password'])) {
+    $_SESSION['studentloggedin'] = true;
+    $_SESSION['userinfo'] = $students['student_id'];
+    ?>
+    <meta http-equiv="refresh" content="2;url=home" />
+    <?php
+  }
+
+  else {
+    ?>
+     <div id="LogIn">
+       <form action="" method="POST">
+         <p> sorry, please check your details and try again.</p>
+         <br>
+         <label>Student ID </label><input type="text" name="student_id"/>
+         <br>
+         <br>
+         <label>Password </label><input type="password" name="password"/>
+         <br>
+         <br>
+         <input type="submit" value="Log In" name="login"/>
+       </form>
+     </div>
+     <div id="HomeVideo">
+     </div>
+  <?php
+  }
+
+}
+else {
+  ?>
+  <div id="LogIn">
+    <form action="" method="POST">
+      <p> Please use your details to Log In</p>
+      <br>
+      <label>Student ID </label><input type="text" name="student_id"/>
+      <br>
+      <br>
+      <label>Password </label><input type="password" name="password"/>
+      <br>
+      <br>
+      <input type="submit" value="Log In" name="login"/>
+    </form>
+  </div>
+  <div id="HomeVideo">
+  </div>
+  <?php
+}
+
+
+ ?>
