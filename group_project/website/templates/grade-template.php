b743993f6a04bf7ac4a2133fe9ef7eb40f7659a2
TraivsBrookes96 <tbrookes96@gmail.com>
2017-03-15 11:33:48 +0000

    Grades template corrected
    Forgot to call getHTML function should now display if there is data and
    someone is logged in

--- group_project/website/templates/grade-template.php
+++ group_project/website/templates/grade-template.php
@@ -4,34 +4,37 @@ $pdo = new PDO('mysql:dbname=wuc;host=127.0.0.1', 'student', 'student');
 if (isset($_SESSION['userinfo']))
 {
   $stmt = find($pdo, 'grades', 'student_id', $_SESSION['userinfo']);
-}
-$table = new TableGenerator();
-$table->setHeadings(['Module ID', 'Assesment 1','Assesment 2','Assesment 3', 'Overall Grade']);
-$table->setColumnTypes(['td','td','td','td','td']);
-foreach($stmt as $row)
-{
-  $average = (($row['grade1'] + $row['grade2'] + $row['grade3']) / 3);
-  if ($average >= 70)
+
+  $table = new TableGenerator();
+  $table->setHeadings(['Module ID', 'Assesment 1','Assesment 2','Assesment 3', 'Overall Grade']);
+  $table->setColumnTypes(['td','td','td','td','td']);
+  foreach($stmt as $row)
   {
-    $grade = 'A';
+    $average = (($row['grade1'] + $row['grade2'] + $row['grade3']) / 3);
+    if ($average >= 70)
+    {
+      $grade = 'A';
+    }
+    else if ($average >= 60)
+    {
+      $grade = 'B';
+    }
+    else if ($average > = 50)
+    {
+      $grade = 'C';
+    }
+    else if ($average >= 40)
+    {
+      $grade = 'D';
+    }
+    else
+    {
+      $grade = 'F';
+    }
+    $table->addRow([$row['module_id'], $row['grade1'], $row['grade2'], $row['grade2'], $grade])
   }
-  else if ($average >= 60)
-  {
-    $grade = 'B';
-  }
-  else if ($average > = 50)
-  {
-    $grade = 'C';
-  }
-  else if ($average >= 40)
-  {
-    $grade = 'D';
-  }
-  else
-  {
-    $grade = 'F';
-  }
-  $table->addRow([$row['module_id'], $row['grade1'], $row['grade2'], $row['grade2'], $grade])
+
+  echo $table->getHTML();
 }
 ?>
 </div>
