c6b93f7808775e52ae3ba137428375866ae6b50a
TraivsBrookes96 <tbrookes96@gmail.com>
2017-03-17 12:34:57 +0000

    Revert "some changes"
    This reverts commit 15d56b820f8b1056d95f77e8b868c39704576617.

--- group_project/website/templates/change-details-template.php
+++ group_project/website/templates/change-details-template.php
@@ -1,29 +1,15 @@
 <div class="fullscreen">
-  <a href=forms>Return to forms</a>
   <p>Please ammend the data below so that it is correct</p>
   <p>Any data changed will be submitted to the admin team to approve.</p>
-  <br>
   <form action="change-details" method="post">
-    <label class="details-form">Firstname</label>
-    <input class="details-form" type="text" name="firstname"/>
-    <label class="details-form" >Surname</label>
-    <input class="details-form"  type="text" name="surname"/>
-    <label class="details-form" >Term Address</label>
-    <input class="details-form"  type="text" name="term_address_line1"/>
-    <input class="details-form"  type="text" name="term_address_line2"/>
-    <label class="details-form" >Home Address</label>
-    <input class="details-form"  type="text" name="haddress"/>
-    <label class="details-form" >Email Address</label>
-    <input class="details-form"  type="text" name="email"/>
-    <label class="details-form" >Mobile Number</label>
-    <input class="details-form" type="text" name="phone-num"/>
+    <label class="details-form">Forename</label><input class="details-form" type="text" name="forename"/>
+    <label class="details-form" >Surname</label><input class="details-form"  type="text" name="surname"/>
+    <label class="details-form" >Term Address</label><input class="details-form"  type="text" name="taddress"/>
+    <label class="details-form" >Home Address</label><input class="details-form"  type="text" name="haddress"/>
+    <label class="details-form" >Email Address</label><input class="details-form"  type="text" name="email"/>
+    <label class="details-form" >Mobile Number</label><input class="details-form" type="text" name="phone-num"/>
     <br>
     <input  class="details-form" type="submit" name="submit" value="Submit For Approval"/>
   </form>
+  <a href=forms>Return to forms</a>
 </div>
-<?php
-
-
-
-
- ?>
