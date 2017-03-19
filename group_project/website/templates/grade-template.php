024340eb50a1369e054c33f86341ca7d4d3af849
TraivsBrookes96 <tbrookes96@gmail.com>
2017-03-19 11:06:25 +0000

    Grades Update
    Grades id now includes module title

--- group_project/website/templates/grade-template.php
+++ group_project/website/templates/grade-template.php
@@ -4,10 +4,16 @@ require '../classes/tablegenerator.php';
 $pdo = new PDO('mysql:dbname=wuc;host=127.0.0.1', 'student', 'student');
 if (isset($_SESSION['userinfo']))
 {
-  $stmt = find($pdo, 'grades', 'student_id', $_SESSION['userinfo']);
+  //$stmt = find($pdo, 'grades', 'student_id', $_SESSION['userinfo']);
+
+  $stmt = $pdo->prepare('SELECT m.module_title, g.module_id, g.grade1, g.grade2, g.grade3 FROM grades g JOIN modules m ON g.module_id = m.module_id WHERE g.student_id = :student_id');
+  $criteria =[
+    'student_id'=>$_SESSION['userinfo']
+  ];
+  $stmt->execute($criteria);
 
   $table = new TableGenerator();
-  $table->setHeadings(['Module ID', 'Assesment 1','Assesment 2','Assesment 3', 'Overall Grade']);
+  $table->setHeadings(['Module ', 'Assesment 1','Assesment 2','Assesment 3', 'Overall Grade']);
   $table->setColumnTypes(['td','td','td','td','td']);
   foreach($stmt as $row)
   {
@@ -40,7 +46,7 @@ if (isset($_SESSION['userinfo']))
     {
       $grade = 'F';
     }
-    $table->addRow([$row['module_id'], $row['grade1'], $row['grade2'], $row['grade2'], $grade]);
+    $table->addRow([$row['module_title']." (".$row['module_id'].")", $row['grade1'], $row['grade2'], $row['grade2'], $grade]);
   }
 
   echo $table->getHTML();
